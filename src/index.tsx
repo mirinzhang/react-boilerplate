5b1f23a081941e87f66a14a65b931dd95b4f9e52
MiRinZhang <noodles@yabing.me>
2017-07-07 14:17:46 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@
  */
 import React from 'react';
 import { render } from 'react-dom';
+import { DatePicker } from 'antd';
 
 const App = (props: any) => {
   return (
     <div>
       <p>React-TS Project</p>
       <p>module name: {props.name}</p>
+      <DatePicker />
     </div>
   );
 };
