0f2be255cecd7dddacf801f2209171e96f317085
张亚兵 <noodles@yabing.me>
2017-07-11 17:18:11 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ const path = require('path'),
         })
         : [ 'style', 'css', 'postcss', 'sass' ],
     vendors = [
-        'antd',
         'react',
         'react-dom',
         'react-redux',
