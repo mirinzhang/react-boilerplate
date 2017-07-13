f69614c98d132ca9a9073031c0b90362b1c642ea
MiRinZhang <noodles@yabing.me>
2017-07-13 14:47:21 +0800

    change loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,13 +3,12 @@
  */
 import React, { Component } from 'react';
 import { Spin, Row, Col } from 'antd';
-
-import './style';
+import styles from './style';
 
 export default class Loading extends Component {
     render() {
         return (
-            <Row className="container">
+            <Row className={styles.container}>
                 <Col span={12} />
                 <Spin tip="加载中…" size="large" />
             </Row>
