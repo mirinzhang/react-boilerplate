45865a4694c7925b54d1e5bde4a252e13e4403df
MiRinZhang <noodles@yabing.me>
2017-12-19 19:49:52 +0800

    change publicPath

--- config/app.conf.js
+++ config/app.conf.js
@@ -10,31 +10,30 @@ module.exports = {
     apps: {
         entry: {
             dev: [
-                `webpack-dev-server/client?http://${HOST}:${_PORT}`,
-                'webpack/hot/only-dev-server',
-                './src/index',
+                `webpack-dev-server/client?http://${HOST}:${_PORT}`, 'webpack/hot/only-dev-server', './src/index'
             ],
             prod: [
-                ...Object.keys(pkg.dependencies).filter(
-                    val => !val.startsWith('@'),
-                ),
-                './src/index',
-            ],
+                ...Object
+                    .keys(pkg.dependencies)
+                    .filter(val => !val.startsWith('@'),),
+                './src/index'
+            ]
         },
         output: {
             path: path.join(__dirname, '../dist'),
             filename: '[name].js',
             chunkFilename: '[name].[chunkhash:6].chunk.js',
             sourceMapFilename: '[name].bundle.map',
+            publicPath: '/'
         },
         devtool: {
             dev: 'eval-cheap-module-source-map',
-            prod: 'hidden-source-map',
-        },
+            prod: 'hidden-source-map'
+        }
     },
     devServer: {
         host: HOST,
-        port: _PORT,
+        port: _PORT
     },
-    proxy: {},
+    proxy: {}
 };
