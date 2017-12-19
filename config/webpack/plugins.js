82562d052318983e5ce03ca221fafb8a57f13a93
MiRinZhang <noodles@yabing.me>
2017-12-19 19:52:43 +0800

    save

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -1,95 +1,91 @@
 /**
  * Created by Min on 2017/8/19.
  */
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const WebpackChunkHash = require('webpack-chunk-hash');
-const DashboardPlugin = require('webpack-dashboard/plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const WebpackNotifierPlugin = require('webpack-notifier');
+const path = require('path'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    WebpackChunkHash = require('webpack-chunk-hash'),
+    DashboardPlugin = require('webpack-dashboard/plugin'), {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin'),
+    WebpackNotifierPlugin = require('webpack-notifier'),
+    __DEV__ = (process.env.NODE_ENV || 'development') === 'development';
 
-const __DEV__ = (process.env.NODE_ENV || 'development') === 'development';
-
-exports.commonPlugins = [
-    new webpack.DefinePlugin({
+exports.commonPlugins = [new webpack.DefinePlugin({
         'process.env': {
-            NODE_ENV: JSON.stringify(__DEV__ ? 'development' : 'production'),
-        },
-    }),
-];
+            NODE_ENV: JSON.stringify(__DEV__
+                ? 'development'
+                : 'production')
+        }
+    })];
 
 exports.devPlugins = [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({
-        title: 'Development',
-        filename: 'index.html',
-        template: 'index.html',
-        inject: true,
-        hash: true,
-    }),
-    new webpack.LoaderOptionsPlugin({
-        debug: true,
-        minimize: false,
-    }),
+    new HtmlWebpackPlugin({title: 'Development', filename: 'index.html', template: 'index.html', inject: true, hash: true}),
+    new webpack.LoaderOptionsPlugin({debug: true, minimize: false}),
     new webpack.DllReferencePlugin({
-        manifest: path.join(__dirname, '../../public/dll', 'manifest.json'),
+        manifest: path.join(__dirname, '../../public/dll', 'manifest.json')
     }),
     new DashboardPlugin(),
     new WebpackNotifierPlugin({
         title: 'Compiled successfully!',
-        message: '🤡' + new Date(),
-    }),
+        message: '🤡' + new Date()
+    })
 ];
 
 exports.prodPlugins = [
-    new CleanWebpackPlugin([ 'dist', 'public' ], { root: path.join(__dirname, '../../') }),
-    new webpack.optimize.CommonsChunkPlugin({
-        names: [ 'vendor', 'manifest' ],
-        filename: 'vendor.bundle.js',
-        minChunks: ({ resource }) =>
-        resource &&
-        resource.indexOf('node_modules') >= 0 &&
-        resource.match(/\.(js|less|scss)$/)
+    new CleanWebpackPlugin([
+        'dist', 'public'
+    ], {
+        root: path.join(__dirname, '../../')
     }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack
+        .optimize
+        .CommonsChunkPlugin({
+            names: [
+                'vendor', 'manifest'
+            ],
+            filename: 'vendor.bundle.js',
+            minChunks: ({resource}) => resource && resource.indexOf('node_modules') >= 0 && resource.match(/\.(js|less|scss)$/)
+        }),
+    new webpack
+        .optimize
+        .ModuleConcatenationPlugin(),
     new webpack.LoaderOptionsPlugin({
         minimize: true,
         debug: false,
         quiet: true,
         options: {
             context: '/'
-        },
+        }
     }),
-    new ExtractTextPlugin({
-        filename: '[name].style.[contenthash].css',
-        disable: false,
-        allChunks: true,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-        sourceMap: true,
-        output: false,
-        compress: {
-            unused: true,
-            dead_code: true,
-            pure_getters: true,
-            warnings: false,
-            screw_ie8: true,
-            conditionals: true,
-            comparisons: true,
-            sequences: true,
-            evaluate: true,
-            join_vars: true,
-            if_return: true,
-        },
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
+    new ExtractTextPlugin({filename: '[name].style.[contenthash].css', disable: false, allChunks: true}),
+    new webpack
+        .optimize
+        .UglifyJsPlugin({
+            sourceMap: true,
+            output: false,
+            compress: {
+                unused: true,
+                dead_code: true,
+                pure_getters: true,
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                comparisons: true,
+                sequences: true,
+                evaluate: true,
+                join_vars: true,
+                if_return: true
+            }
+        }),
+    new webpack
+        .optimize
+        .AggressiveMergingPlugin(),
     new webpack.HashedModuleIdsPlugin(),
     new WebpackChunkHash(),
     new HtmlWebpackPlugin({
@@ -104,10 +100,8 @@ exports.prodPlugins = [
             removeAttributeQuotes: true,
             minifyJS: true,
             minifyCSS: true,
-            minifyURLs: true,
-        },
+            minifyURLs: true
+        }
     }),
-    new BundleAnalyzerPlugin({
-        analyzerMode: 'static'
-    }),
+    new BundleAnalyzerPlugin({analyzerMode: 'static'})
 ];
